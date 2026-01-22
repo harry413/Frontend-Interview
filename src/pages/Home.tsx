30cd3ba7292419e4538d35cfa707e5fa7a260a43
Sachin patidar <sachinpatidar413@gmail.com>
2026-01-22 16:44:15 +0530

    home page changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   
   return (
     <div className=" h-screen  py-8 px-4 gap-4 bg-gradient-to-b from-gray-100 via-gray-200 to-gray-300">
-      <h1 className="text-center text-4xl font-bold font-sans">Welcome to the latest and intresting blogs web </h1>
+      <h1 className="text-center text-4xl font-bold font-sans mt-10">Welcome to the latest and intresting blogs web </h1>
     </div>
   );
 };
